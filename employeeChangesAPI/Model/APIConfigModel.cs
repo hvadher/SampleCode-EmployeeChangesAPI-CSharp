4a4dd6ae181a5bbe03526be2287e64a579bddc29
Bryce Deneen <Bryce_Deneen@ultimatesoftware.com>
2018-03-23 08:44:13 -0400

    initial commit

---
+++ employeeChangesAPI/Model/APIConfigModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace employeeChangesAPI.Model
+{
+    public class APIConfigModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string APIUrlBase { get; set; }
+        public string UserAPIKey { get; set; }
+        public string CustomerAPIKey { get; set; }
+    }
+}
---
+++ employeeChangesAPI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using employeeChangesAPI.Model;
+using Newtonsoft.Json;
+
+namespace employeeChangesAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            APIConfigModel sampleConfig = new APIConfigModel();
+            sampleConfig.APIUrlBase = "{{APIURLHERE}}";
+            sampleConfig.UserName = "{{USERNAMEHERE}}";
+            sampleConfig.Password = "{{APIPASSWORDHERE}}";
+            sampleConfig.CustomerAPIKey = "{{CUSTOMERAPIKEYHERE}}";
+            sampleConfig.UserAPIKey = "{{USERAPIKEYHERE}}";
+
+
+            File.WriteAllText(@"apiConfig.json", JsonConvert.SerializeObject(sampleConfig));
+
+            */
+
+            Model.APIConfigModel apiConfig = JsonConvert.DeserializeObject<APIConfigModel>(File.ReadAllText(@"testAccount.json"));
+
+
+        }
+    }
+}
